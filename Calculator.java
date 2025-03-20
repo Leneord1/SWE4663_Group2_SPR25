95509059793d941bb21c9bbe57b11ad7a12568cc
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2025-03-19 21:42:47 -0400

    Update Calculator.java
    Added documentation

--- Calculator.java
+++ Calculator.java
@@ -3,18 +3,22 @@ import java.lang.Math;
 public class Calculator {
 
     public double addition(double num1, double num2){
+        //Does addition
         return num1 + num2;
     }
 
     public double subtraction(double num1, double num2){
+        //does subtraction
         return num1 - num2;
     }
 
     public double multiplication(double num1, double num2){
+        //does multiplication
         return num1 * num2;
     }
 
     public double division(double num1, double num2) {
+        //does division
         if (num2 == 0) {
             return -1;
         } else {
@@ -23,10 +27,12 @@ public class Calculator {
     }
 
     public double modulo(double num1, double num2){
+        //does modulo calculations
         return num1 % num2;
     }
 
     public double exponent(double num1, double num2){
+        //does exponential calculations
         if(num2 != 0){
             return (num1 * exponent(num1, num2 - 1));
         } else if (num2 < 0) {
