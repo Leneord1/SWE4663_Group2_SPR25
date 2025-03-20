41de27a6b501bd2e83039871718282309cf31982
Leneord1 <71476576+Leneord1@users.noreply.github.com>
2025-03-19 21:46:23 -0400

    Update driver.java
    Added documentation

--- driver.java
+++ driver.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 import java.io.*;
 import java.awt.GraphicsEnvironment;
 import java.net.URISyntaxException;
+
 public class driver {
+    
     public static void main(String[] args){
+        //All the variables that will be used
         Scanner userInput = new Scanner(System.in);
         double num1;
         double num2;
@@ -14,6 +17,7 @@ public class driver {
         quadFormula qFormula = new quadFormula();
 
         do {
+            //First loop
             System.out.print("""
                     What operation would you like to do?
                     1. +
@@ -26,7 +30,7 @@ public class driver {
             operator = userInput.nextInt();
 
             switch (operator) {
-                case 1:
+                case 1: //does addition
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -36,7 +40,7 @@ public class driver {
                     System.out.println(calculator.addition(num1, num2));
                     break;
 
-                case 2:
+                case 2: //does subtraction
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -46,7 +50,7 @@ public class driver {
                     System.out.println(calculator.subtraction(num1, num2));
                     break;
 
-                case 3:
+                case 3:  //does multiplicaiton
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -56,7 +60,7 @@ public class driver {
                     System.out.println(calculator.multiplication(num1, num2));
                     break;
 
-                case 4:
+                case 4:  //does division
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -66,7 +70,7 @@ public class driver {
                     System.out.println(calculator.division(num1, num2));
                     break;
 
-                case 5:
+                case 5:  //does exponents
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -75,7 +79,7 @@ public class driver {
 
                     System.out.println(calculator.exponent(num1, num2));
                     break;
-                case 6:
+                case 6:  //does modulo computations
                     System.out.println("What is your first number? ");
                     num1 = userInput.nextDouble();
 
@@ -85,12 +89,12 @@ public class driver {
                     System.out.println(calculator.modulo(num1, num2));
                     break;
 
-                case 9:
+                case 9:  //breaks the loop
                     loop = false;
                     break;
 
             }
         }
-        while(loop);
+        while(loop);  
         }
     }
